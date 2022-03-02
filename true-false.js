9225659fe2621807e2872003db33bbf98965efaf
Raihan-hr <raihanhr91@gmail.com>
2022-03-02 21:22:35 +0600

    True false

---
+++ true-false.js
@@ -0,0 +1,24 @@
+// About true and false
+
+// All false
+// 0
+// "";
+// undefined
+// null
+// NaN
+// false
+
+// All true
+// '0'
+// " "
+// {}
+// 'false'
+
+const name = " "
+
+if (name) {
+    console.log("condition is true")
+}
+else {
+    console.log("condition is false")
+}
