0f5f2b69b5e08884717bfed3f254b2f193f711fa
Raihan-hr <raihanhr91@gmail.com>
2022-03-03 01:50:44 +0600

    Null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,29 @@
+// All undefined
+
+let pakhi;
+
+function add (num, num2) {
+    console.log(num+num2)
+    // if i don't write return that time i will get undefined
+     return 
+    
+    // !If i write only return i will get undefined
+
+}
+
+
+function add(num, num2) {
+    console.log(num, num2)
+}
+
+const result = add(10)
+
+
+const premik = {name: "Rahiim mia", phone: 4145545}
+console.log(premik.gf);
+
+let fun = undefined;
+
+// about null
+
+// i can set anywhere the null
