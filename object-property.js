d0ed1a7ab03e8aeb2a39ea62cbe45868ca6471b9
Raihan-hr <raihanhr91@gmail.com>
2022-03-04 00:22:54 +0600

    Added something new

--- object-property.js
+++ object-property.js
@@ -7,10 +7,12 @@ const students = [
     {id: 41, name:'yeaseen sani'}
 ];
 
-const names = students.map( sName => sName.name);
-const ids = students.map( sName => sName.id);
-const bigger = students.filter(s => s.id > 30);
-const biggerOne = students.find(s => s.id > 30)
-console.log(biggerOne)
+
+
+// const names = students.map( sName => sName.name);
+// const ids = students.map( sName => sName.id);
+// const bigger = students.filter(s => s.id > 30);
+// const biggerOne = students.find(s => s.id > 30)
+// console.log(biggerOne)
 
 
---
+++ scope.js
@@ -0,0 +1,12 @@
+let bonus = 20;
+
+function sum (first, second){
+    let result = first + second + bonus;
+    console.log(result, bonus);
+    return result;
+    
+}
+const output = sum (5, 5);
+console.log(bonus);
+console.log(output);
+
