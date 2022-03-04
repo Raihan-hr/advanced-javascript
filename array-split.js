e08d93a89a803935c9b5419ed45aa4f7bf643a03
Raihan-hr <raihanhr91@gmail.com>
2022-03-04 12:56:27 +0600

    Something new

---
+++ array-split.js
@@ -0,0 +1,13 @@
+const numbers = [1, 2, 3, 5, 6, 7];
+
+// const part = numbers.slice(1, 4);
+// const removed = numbers.splice(1,4,);
+// console.log(removed);
+
+// Slice start to end previous. It won't be removed from array!
+// splice start to selected numbers it will totally removed from array. also i can add some new items there!
+
+const together = numbers.join(" ");
+console.log(together);
+
+// Whatever i add on join it will add beside array items!
